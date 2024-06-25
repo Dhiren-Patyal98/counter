5b1595be587da419a955c9ccb975595a8f6b542b
Dhiren <patyaldhiren@gmail.com>
2024-06-25 13:17:01 +0530

    COUNTER APP | 25-06-2024 | 1:17 PM |

--- src/CouterApp.js
+++ src/CouterApp.js
@@ -11,7 +11,7 @@ export default function CouterApp({ setCount}) {
   <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5z"/>
   <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1"/>
 </svg> </span>
-       <span style={{marginLeft:"10px" ,padding:"0", margin:"0"}} onClick={() => setCount((count) => count - 1)}><svg xmlns="http://www.w3.org/2000/svg" width="66" height="66" fill="black" class="bi bi-file-minus" viewBox="0 0 16 16">
+       <span style={{marginLeft:"10px" ,padding:"0", margin:"0"}} onClick={() => setCount((count) => (count > 0 ? count-1 : 0))}><svg xmlns="http://www.w3.org/2000/svg" width="66" height="66" fill="black" class="bi bi-file-minus" viewBox="0 0 16 16">
   <path d="M5.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5"/>
   <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1"/>
 </svg></span>
